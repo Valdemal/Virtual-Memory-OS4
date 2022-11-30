545adc5a25800906e6824bd7186fbf7e92f30b9c
vovan <vovanex12@gmail.com>
2022-11-30 22:02:26 +0300

    Initial commit

---
+++ HeapManager.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <atomic>
+#include <sys/mman.h>
+
+#include "HeapManager.h"
+
+HeapManager::chunk_t *HeapManager::chunk_head = nullptr;
+
+void *HeapManager::allocate(size_t size) {
+    const auto ALIGNED_SIZE = align_up(size, 16);
+    const auto ALLOC_SIZE = get_alloc_size(ALIGNED_SIZE);
+
+#if DEBUG
+    std::cout << "Aligned size " << ALIGNED_SIZE << std::endl;
+#endif
+
+    if (ALLOC_SIZE >= MAX_ALLOC_SIZE) {
+#if DEBUG
+        std::cout << "Big size. Trying to allocate with map!" << std::endl;
+#endif
+        return map_allocate(ALIGNED_SIZE, 0)->get_memory_ptr();
+    }
+
+    auto chunk_ptr = chunk_head;
+    while (chunk_ptr != nullptr) {
+        if (chunk_ptr->size >= size and chunk_ptr->is_free) {
+#if DEBUG
+            std::cout << "Chunk reused!" << std::endl;
+#endif
+            chunk_ptr->is_free = false;
+            return chunk_ptr->get_memory_ptr();
+        }
+
+        if (chunk_ptr->next == nullptr)
+            break;
+    }
+
+    chunk_ptr = (chunk_t *) (sbrk(intptr_t(ALLOC_SIZE)));
+
+    if (chunk_ptr == nullptr) {
+#if DEBUG
+        std::cout << "Failed to alloc with sbrk! Trying mmapp alloc" << std::endl;
+#endif
+        chunk_ptr = map_allocate(size, ALLOC_SIZE);
+    }
+
+    *chunk_ptr = chunk_t(ALIGNED_SIZE);
+    chunk_ptr->prev = chunk_head;
+
+    if (chunk_head == nullptr)
+        chunk_head = chunk_ptr;
+
+#if DEBUG
+    std::cout << "Chunk inserted in list!" << std::endl;
+#endif
+
+    return chunk_ptr->get_memory_ptr();
+}
+
+size_t HeapManager::align_up(size_t num, size_t align) {
+    return (num + align - 1) & ~(align - 1);
+}
+
+size_t HeapManager::get_alloc_size(size_t size) {
+    return size + sizeof(chunk_t);
+}
+
+HeapManager::chunk_t *HeapManager::map_allocate(size_t size, size_t alloc_size) {
+    auto new_chunk = (chunk_t *) mmap(
+            nullptr, alloc_size, PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0
+    );
+
+    if (new_chunk == MAP_FAILED) {
+#if DEBUG
+        std::cout << "Chunk mapping failed!" << std::endl;
+#endif
+        return nullptr;
+    }
+
+    *new_chunk = chunk_t(size, true);
+
+#if DEBUG
+    std::cout << "Chunk mapping ok!" << std::endl;
+#endif
+
+
+    return new_chunk;
+}
+
+void HeapManager::free(void *ptr) {
+    auto chunk_ptr = chunk_t::get_from_memory(ptr);
+
+    if (chunk_ptr->is_free) {
+#if DEBUG
+        std::cout << "Chunk is already free!" << std::endl;
+#endif
+
+        return;
+    }
+
+    if (chunk_ptr->is_mapped) {
+        int status = munmap(chunk_ptr, get_alloc_size(chunk_ptr->size));
+
+#if DEBUG
+        if (status == 0) {
+            std::cout << "Unmap done!" << std::endl;
+        } else {
+            std::cout << "Unmap failed with code" << status << '!' << std::endl;
+        }
+#endif
+    } else {
+
+#if DEBUG
+        std::cout << "Chunk marked as free!" << std::endl;
+#endif
+        chunk_ptr->is_free = true;
+    }
+}
+
+HeapManager::chunk_t::chunk_t(size_t size, bool mapped)
+        : size(size), next(nullptr), prev(nullptr), is_free(false), is_mapped(mapped) {}
+
+void *HeapManager::chunk_t::get_memory_ptr() const {
+    return (void *) ((char *) this + sizeof(chunk_t));
+}
+
+HeapManager::chunk_t *HeapManager::chunk_t::get_from_memory(void *ptr) {
+    return (chunk_t *) ((char *) ptr - sizeof(chunk_t));
+}
---
+++ HeapManager.h
@@ -0,0 +1,48 @@
+#ifndef HEAP_MANAGER_H
+#define HEAP_MANAGER_H
+
+
+#include <cstddef>
+#include <cstdint>
+
+#define DEBUG true
+
+static const size_t MAX_ALLOC_SIZE = 33554432;
+
+class HeapManager {
+public:
+    static void *allocate(size_t size);
+
+    static void free(void *ptr);
+
+private:
+    struct chunk_t {
+        size_t size;
+
+        bool is_mapped;
+        bool is_free;
+
+        chunk_t *next;
+        chunk_t *prev;
+
+        explicit chunk_t(size_t size, bool mapped = false);
+
+        // Возвращает указатель на данные по chunk
+        inline void* get_memory_ptr() const;
+
+        // Возвращает указатель на chunk по данным ptr
+        static inline chunk_t* get_from_memory(void *ptr);
+    };
+
+    static chunk_t* chunk_head;
+
+    // Возможно здесь будет подводный камень
+    static inline size_t align_up(size_t num, size_t align);
+
+    static inline size_t get_alloc_size(size_t size);
+
+    static HeapManager::chunk_t *map_allocate(size_t size, size_t alloc_size);
+};
+
+
+#endif //HEAP_MANAGER_H
